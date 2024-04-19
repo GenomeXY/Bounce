102f2c5b83cc23bccb98310991bd64cc9f5e63f0
GigaOrts <ortsortskhoev95@gmail.com>
2024-04-19 15:20:42 +0300

    ball camera motion fix

--- Assets/_Source/Player/Scripts/Behaviours/FollowBehaviour.cs
+++ Assets/_Source/Player/Scripts/Behaviours/FollowBehaviour.cs
@@ -5,10 +5,12 @@ public class FollowBehaviour : MonoBehaviour
     [SerializeField] private Transform _target;
     [SerializeField] private Vector3 _offset;
     [SerializeField] private bool _repeatRotation;
-    
+    [SerializeField] private bool _repeatTranslation;
+
     private void LateUpdate()
     {
-        transform.position = _target.position + _offset;
+        if (_repeatTranslation)
+            transform.position = _target.position + _offset;
 
         if (_repeatRotation)
             transform.rotation = _target.rotation;
